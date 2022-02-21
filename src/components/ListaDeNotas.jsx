dadad4507b351e62372319536edc1ada50fd7d28
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-02-21 14:54:51 -0300

    Criando a lista de notas

---
+++ src/components/ListaDeNotas.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+function ListaDeNotas() {
+  return(
+    <>
+      <ul>
+        <li>
+          <section>
+            <header>
+              <h3>Título</h3>
+            </header>
+            <p>Escreva sua nota</p>
+          </section>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <section>
+            <header>
+              <h3>Título</h3>
+            </header>
+            <p>Escreva sua nota</p>
+          </section>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <section>
+            <header>
+              <h3>Título</h3>
+            </header>
+            <p>Escreva sua nota</p>
+          </section>
+        </li>
+      </ul>
+    </>
+  );
+}
+
+export default ListaDeNotas;
