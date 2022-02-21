f50800b39176a41d30725648046718f362defbae
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-02-21 15:10:34 -0300

    Importando os componentes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ListaDeNotas from './components/ListaDeNotas';
+import FormularioCadastro from './components/FormularioCadastro';
 
 function App() {
   return (
     <>
-      <form>
-        <input type='text' placeholder='title' />
-        <textarea placeholder='Escreva sua nota' />
-        <button>Criar nota</button>
-      </form>
+      <FormularioCadastro />
       <ListaDeNotas />
     </>
   );
